3ddf61923dcf3be356002cda0ea746dca9523a9d
dianaostik <dianastud12@gmail.com>
2022-10-27 17:02:43 +0300

    1com

--- js/main.js
+++ js/main.js
@@ -8,3 +8,21 @@ for (let i = 0; i < 30; i++) {
     console.log(items);
 }
 
+
+
+
+// ------ 2 uzduotis
+console.log("--------- 2 uzduotis ---------");
+
+//  a)
+
+let sum = 0;
+for (let i = 0; i < 30; i++) {
+    let items = [Math.round(5 + Math.random() * 20)];
+    if (items > 10) {
+        sum++;  
+    }
+    console.log(items);
+}
+ console.log(sum);
+
