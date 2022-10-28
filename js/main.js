52f4817de40aaf287b346f47e5bad9d794cb55a2
dianaostik <dianastud12@gmail.com>
2022-10-28 09:47:04 +0300

    com2

--- js/main.js
+++ js/main.js
@@ -2,13 +2,14 @@
 // ------ 1 uzduotis
 console.log("--------- 1 uzduotis ---------");
 
-
-for (let i = 0; i < 30; i++) {
-    let items = [Math.round(5 + Math.random() * 20)];
-    console.log(items);
+rnd2dArr = [];
+for (let i = 0; i < 10; i++) {
+    rnd2dArr[i] = [];
+    for (let a = 0; a < 3; a++) {
+        rnd2dArr[i][a] = 5 + Math.round(Math.random() * 20);
+    }
 }
-
-
+console.log(rnd2dArr);
 
 
 // ------ 2 uzduotis
@@ -17,12 +18,14 @@ console.log("--------- 2 uzduotis ---------");
 //  a)
 
 let sum = 0;
-for (let i = 0; i < 30; i++) {
-    let items = [Math.round(5 + Math.random() * 20)];
-    if (items > 10) {
-        sum++;  
+for (let b = 0; b < rnd2dArr.length; b++) {
+    for (let c = 0; c < rnd2dArr[b].length; c++) {
+        if (rnd2dArr[b][c] > 10) {
+            sum ++;
+        }
     }
-    console.log(items);
 }
- console.log(sum);
+console.log(sum);
+
+//b
 
