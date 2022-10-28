0a444cdb4f4d835f5ddf60c9eb7c8d5cd9f121b4
dianaostik <dianastud12@gmail.com>
2022-10-28 10:36:08 +0300

    2commit

--- js/main.js
+++ js/main.js
@@ -29,3 +29,34 @@ console.log(sum);
 
 //b
 
+let biggest = 0;
+for (let d = 0; d < rnd2dArr.length; d++) {
+    for (let e = 0; e < rnd2dArr[d].length; e++) {
+        if (rnd2dArr[d][e] > biggest) {
+            biggest = rnd2dArr[d][e];
+        }
+    }
+}
+console.log(biggest);
+
+//c
+
+let summa = 0;
+for (let g = 0; g < rnd2dArr.length; g++) {
+    for (let f = 0; f < rnd2dArr[g].length; f++) {
+            summa += rnd2dArr[g][f];
+    }
+}
+console.log(summa);
+
+
+//d
+
+let newArray = [];
+for (let h = 0; h < rnd2dArr.length; h++) {
+    newArray[h] = [];
+    for (let j = 0; j < rnd2dArr[h].length; j++) {
+         newArray[h][j] = rnd2dArr[h][j] - j;   
+    }
+}
+console.log(newArray);
